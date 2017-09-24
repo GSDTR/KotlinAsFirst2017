fe61e0b0cf6a0ed511fc0e9ecc3bc77306d1ac49
GSDTR <ilnur222@yahoo.com>
2017-09-24 23:48:45 +0300

    Решено seconds, lengthInMetres, angleInRadian, trackLength

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -85,7 +85,7 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double {
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
 fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double  {
-    val leng = sqrt((sqr(abs(x1 - x2))) + (sqr(abs(y1 - y2))))
+    val leng = sqrt(sqr(abs(x1 - x2)) + sqr(abs(y1 - y2)))
     return leng
 }
 
