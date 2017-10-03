6bf8345d3d03344215c6198d96ae21e08149845e
GSDTR <ilnur222@yahoo.com>
2017-10-03 19:32:51 +0300

    Решено seconds, lengthInMetres, angleInRadian, trackLength

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -60,7 +60,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int  = hours * 3600 + minut
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes * 48 + 48 / 3.0 * arshins + vershoks) * 4.445 / 100.0
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+        (sagenes * 48 + 48 / 3.0 * arshins + vershoks) * 4.445 / 100.0
 
 /**
  * Тривиальная
@@ -93,9 +94,8 @@ fun thirdDigit(number: Int): Int = number / 100 % 10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = if (hoursArrive < hoursDepart) {
-        24 * 60 - hoursDepart * 60 - minutesDepart + hoursArrive * 60 + minutesDepart }
-    else { hoursArrive * 60 + minutesArrive - hoursDepart * 60 - minutesDepart }
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+        24 * 60 - hoursDepart * 60 - minutesDepart + hoursArrive * 60 + minutesDepart
 
 /**
  * Простая
